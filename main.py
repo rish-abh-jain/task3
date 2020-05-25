5bf518ee2c1080d5461af7eea3abf96149197125
rish-abh-jain <60362872+rish-abh-jain@users.noreply.github.com>
2020-05-25 17:46:00 +0530

    vhvj

--- main.py
+++ main.py
@@ -1,36 +1,77 @@
-# Import essentials
 import keras
 from keras.datasets import mnist
+from _future_ import print_function
 from keras.models import Sequential
-from keras.utils.np_utils import to_categorical
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras.backend import clear_session
-import numpy
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import os
 
-# Load Model 
-(train_X , train_y), (test_X , test_y) = mnist.load_data("mymnist.data")
-# Reshape data and change type
-test_X = test_X.reshape(-1 , 28*28)
-train_X = train_X.reshape(-1 ,  28*28)
-test_X = test_X.astype("float32")
-train_X = train_X.astype("float32")
-# One hot encoding 
-test_y = to_categorical(test_y)
-train_y = to_categorical(train_y)
+def model_train(epoch,n):
+    batch_size = 32
+    num_classes = 10
+    epochs = epoch
 
-model = Sequential()
-model.add(Dense(units = 20 , input_dim = 28*28 , activation = 'relu'))
-model.add(Dense(units=60 , input_dim = 28*28 , activation = 'relu'))
-model.add(Dense(units=10 , input_dim = 28*28 , activation = 'softmax'))
-model.compile( optimizer= "Adam" , loss='categorical_crossentropy', 
-             metrics=['accuracy'] )
-fit_model = model.fit(train_X ,  train_y , epochs = 2 , verbose =  False)
+    # input image dimensions
+    img_rows, img_cols = 28, 28
 
-text = fit_model.history
-accuracy = text['accuracy'][1] * 100
-accuracy = int(accuracy)
-f= open("accuracy.txt","w+")
-f.write(str(accuracy))
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    model = Sequential()
+    
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    if n>1:
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    a=score[1]*100
+    model.save("model.h5")
+    os.system("mv /model.h5 /root")
+    return a
+
+no_epoch=1
+no_layer=1
+accuracy_train_model=model_train(no_epoch,no_layer)
+f = open("accuracy.txt","w+")
+f.write(str(accuracy_train_model))
 f.close()
-print("Accuracy for the model is : " , accuracy ,"%")
+os.system("mv /accuracy.txt /root")
